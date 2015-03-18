ec559b84dc44c199663944a3927ba9ff8ae59ee8
Kevin Bath <bathalex210@gmail.com>
2015-03-17 22:19:09 -0400

    Changed variable

--- functions/startup.php
+++ functions/startup.php
@@ -12,7 +12,7 @@
 		while ($data = pg_fetch_object($result)) {
 			echo '<form action="editstartup.php" method="post" accept-charset="UTF-8">';
 			echo '<input type="hidden" name="title" id="title" value="'.$data->title.'"/>';
-			if (strcmp($_SESSION['email'],$email)==0) {
+			if (strcmp($_SESSION['email'],$data->email)==0) {
 				echo '<button type="submit"><span class="fa fa-pencil fa-2x"></span></button>';
 			}
 			if (isset($_SESSION['email'])) {
