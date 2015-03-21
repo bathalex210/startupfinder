97b5b143ca9b7d2a85a2670c1acd90560546f775
Kevin Bath <bathalex210@gmail.com>
2015-03-21 11:16:54 -0400

    Changed variable name

--- restapi.php
+++ restapi.php
@@ -7,9 +7,9 @@
 	function bestIdeas($k, $dateStart, $dateEnd) {
 		$dbconn = pg_connect("host=ec2-23-23-215-150.compute-1.amazonaws.com dbname=d2psqpda41ih1k user=tfqyqshbouweik password=P3mnTBRoi6sqF6oqcvU3ruO2kS")
 			or die('Could not connect: ' . pg_last_error());
-		$query = "SELECT * FROM (SELECT startup.title,description,industry,startup.email,startup.date,count(rating) FROM startup,likes WHERE startup.title=likes.title AND rating='like' GROUP BY startup.title,description,industry,startup.email,startup.date ORDER BY count(rating) DESC LIMIT $k) AS bestideas WHERE date<='$dateEnd' AND date>='$startDate' ";
+		$query = "SELECT * FROM (SELECT startup.title,description,industry,startup.email,startup.date,count(rating) FROM startup,likes WHERE startup.title=likes.title AND rating='like' GROUP BY startup.title,description,industry,startup.email,startup.date ORDER BY count(rating) DESC LIMIT $k) AS bestideas WHERE date<='$dateEnd' AND date>='$dateStart'";
 		echo $query;
-		/*$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+		$result = pg_query($query) or die('Query failed: ' . pg_last_error());
 		echo "{";
 		while ($data = pg_fetch_object($result)) {
 			echo "\"title\":$data->title,
@@ -18,6 +18,6 @@
 					\"email\":$data->email,
 					\"date\":$data->date";
 		}
-		echo "}";*/
+		echo "}";
 	}
 ?>
