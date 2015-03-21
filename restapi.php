d957a6bf8cc8f5d5165fb4147d6c6e372e5dae1e
Kevin Bath <bathalex210@gmail.com>
2015-03-21 11:04:39 -0400

    Test

--- restapi.php
+++ restapi.php
@@ -6,12 +6,9 @@
 		}
 	}
 	function bestIdeas($k, $dateStart, $dateEnd) {
-		$dbconn = pg_connect("host=ec2-23-23-215-150.compute-1.amazonaws.com dbname=d2psqpda41ih1k user=tfqyqshbouweik password=P3mnTBRoi6sqF6oqcvU3ruO2kS")
+		/*$dbconn = pg_connect("host=ec2-23-23-215-150.compute-1.amazonaws.com dbname=d2psqpda41ih1k user=tfqyqshbouweik password=P3mnTBRoi6sqF6oqcvU3ruO2kS")
 			or die('Could not connect: ' . pg_last_error());
-		$query = "
-		SELECT * FROM (
-			SELECT startup.title,description,industry,startup.email,startup.date,count(rating) FROM startup,likes WHERE startup.title=likes.title AND rating='like' GROUP BY startup.title,description,industry,startup.email,startup.date ORDER BY count(rating) DESC
-		) AS bestideas WHERE date<='$dateEnd' AND date>='$startDate' LIMIT $k;";
+		$query = "SELECT * FROM (SELECT startup.title,description,industry,startup.email,startup.date,count(rating) FROM startup,likes WHERE startup.title=likes.title AND rating='like' GROUP BY startup.title,description,industry,startup.email,startup.date ORDER BY count(rating) DESC) AS bestideas WHERE date<='$dateEnd' AND date>='$startDate' LIMIT $k;";
 		$result = pg_query($query) or die('Query failed: ' . pg_last_error());
 		echo "{";
 		while ($data = pg_fetch_object($result)) {
@@ -21,6 +18,6 @@
 					\"email\":$data->email,
 					\"date\":$data->date"
 		}
-		echo "}";
+		echo "}";*/
 	}
 ?>
